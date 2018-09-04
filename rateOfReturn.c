3f7cb7be39cb65c719cfa8531e957749acdba12b
C <kkwertz11@gmail.com>
2018-09-03 22:11:53 -0500

    actually made it accept proper input

--- rateOfReturn.c
+++ rateOfReturn.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 int main(void){
-	double initial_value, final_value, years;
+	double initial_value, final_value, years, weeks, days;
 	
 	printf("Initial value?: ");
 	scanf("%lf",&initial_value);
@@ -11,10 +11,14 @@ int main(void){
 	scanf("%lf",&final_value);
 	printf("Years?: ");
 	scanf("%lf",&years);
-	
+	printf("Weeks?: ");
+	scanf("%lf",&weeks);
+	printf("Days?: ");
+	scanf("%lf",&days);
+	years=years + (weeks/52) + (days/365)	
 	printf("Initial value: $%.2lf\n",initial_value);
 	printf("Final value:   $%.2lf\n",final_value);
-	printf("Years:           %.2lf\n",years);
+	printf("Years:           %lf\n",years);
 
 	final_value = ((pow((final_value/initial_value),(1/years)))-1)*100;
 	printf("Annualized rate of return: %lf%%\n",final_value);
